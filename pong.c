62e7145caa0f460bef0f156b7bd521b9f8545fd3
Daniel-Page <dwpage98@gmail.com>
2018-10-10 11:58:42 +1300

    Update pong.c

--- pong.c
+++ pong.c
@@ -116,11 +116,11 @@ void pixel_movement(void)
 {
     if (counter_pixel == (PACER_RATE / PIXEL_RATE)) {
         // Slider rebound
-        if ((pixel_x == 3) && ((pixel_y == 6))) {
-             if (movement_state == NE) {
-                movement_state = SW;
-            } else if (movement_state == SE){
-                movement_state = NW;
+        if ((pixel_x == 3) && ((pixel_y == 6) || (pixel_y == 0)) && ((pixel_y == row) || (pixel_y == row+1) || (pixel_y == row-1))) {
+             if (movement_state == NW) {
+                movement_state = SE;
+            } else if (movement_state == SW){
+                movement_state = NE;
             }
         } else if (((pixel_x == 3 && pixel_y == row) ||
                 (pixel_x == 3 && pixel_y == row+1) ||
